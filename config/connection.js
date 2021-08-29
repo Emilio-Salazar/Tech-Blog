7c990284c9489e6afdd560736ddd1840ffd6171f
E Lazar <emiliosalazar4@gmail.com>
2021-08-28 20:09:03 -0500

    added server code

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,13 @@
 const Sequelize = require('sequelize');
-const sequelize = process.env.JAWSDB_URL
+
+require('dotenv').config()
+
+let sequelize = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL)
-  : new Sequelize('sequelize_library', 'root', '', {
+  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
       host: 'localhost',
-      port: 3306,
       dialect: 'mysql',
-      pool: {
-        max: 5,
-        min: 0,
-        idle: 10000
-      }
+      port: 3306
     });
-    module.exports = sequelize;
+
+module.exports = sequelize;
--- server.js
+++ server.js
@@ -1,18 +1,44 @@
+const path = require('path');
 const express = require('express');
-const sequelize = require('./app/config/connection');
-const routes = require('./app/routes');
+// require sessions
+const session = require('express-session');
+const exphbs = require('express-handlebars');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+const sequelize = require('./config/connection');
 
-app.use(express.static('app/public'));
-app.use(routes);
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const sess = {
+  secret: process.env.SESSION_SECRET,
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
+
+app.use(session(sess));
+
+
+
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+const helpers = require('./utils/helpers');
+const hbs = exphbs.create({ helpers });
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(require('./controllers/'));
 
 sequelize.sync({ force: false }).then(() => {
-    app.listen(PORT, function() {
-      console.log('App listening on PORT ' + PORT);
-    });
+  app.listen(PORT, function () {
+    console.log('App listening on PORT ' + PORT);
   });
+});
